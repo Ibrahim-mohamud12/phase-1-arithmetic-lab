51b877010fdd45bca76850a19c00ba1662b0ad77
ibrahim <ibrahim.mohamud1@student.moringaschool.com>
2025-03-10 11:31:47 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// index.js
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+module.exports = { multiply };
+
+// index.js
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
+
+module.exports = { random };
+
+
+
+
